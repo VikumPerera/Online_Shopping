119c0d09ef238d5574f68c213c54f22626904854
VikumPerera <32023231+VikumPerera@users.noreply.github.com>
2017-11-20 15:48:16 +0530

    Environment Created Successfully

---
+++ onlineShopping/src/main/java/com/vkvikingss/onlineShopping/controller/PageController.java
@@ -0,0 +1,22 @@
+package com.vkvikingss.onlineShopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	@RequestMapping(value={"/","/home","/index"})
+	public ModelAndView index(){
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting","Welcome to Spring MVC");
+		return mv;
+	}
+	@RequestMapping(value={"test"})
+	public ModelAndView test(@RequestParam(value="greeting", required=false)String greeting){
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("Welcome to world.....");
+		return mv;
+	}
+}
